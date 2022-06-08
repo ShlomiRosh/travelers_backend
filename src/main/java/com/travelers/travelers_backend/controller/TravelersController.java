c64f42f5774066c28b74f6f14414292f143246b6
ShlomiRosh <shalom053@gmail.com>
2022-06-08 09:49:14 +0300

    with swagger

--- src/main/java/com/travelers/travelers_backend/controller/TravelersController.java
+++ src/main/java/com/travelers/travelers_backend/controller/TravelersController.java
@@ -1,4 +1,16 @@
 package com.travelers.travelers_backend.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/travelers")
 public class TravelersController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<?> hello() {
+        return ResponseEntity.ok("Hello World!");
+    }
 }
